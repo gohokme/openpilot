442cbe69c9e4f3fd60c42e79c77f710a0f22e02d
gohokme <111654745+gohokme@users.noreply.github.com>
2025-03-28 21:13:43 +0900

    Update params.cc

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -314,6 +314,8 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"AutoResLimitTime", PERSISTENT},
     {"VCurvSpeedC", PERSISTENT},
     {"VCurvSpeedT", PERSISTENT},
+    {"VCurvSpeedCMPH", PERSISTENT},
+    {"VCurvSpeedTMPH", PERSISTENT},
     {"OCurvSpeedC", PERSISTENT},
     {"OCurvSpeedT", PERSISTENT},
     {"OSMCustomSpeedLimitC", PERSISTENT},
@@ -392,6 +394,10 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"CruiseGapBySpdOn", PERSISTENT},
     {"CruiseGapBySpdSpd", PERSISTENT},
     {"CruiseGapBySpdGap", PERSISTENT},
+    {"CruiseSetwithRoadLimitSpeedEnabled", PERSISTENT},
+    {"CruiseSetwithRoadLimitSpeedOffset", PERSISTENT},
+    {"OPKRDebug", PERSISTENT},
+    {"SetSpeedFive", PERSISTENT},
 };
 
 } // namespace
